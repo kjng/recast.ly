a5f2d1a35fc88dc18e914a1e312da27277a9fbbe
Kevin Jang <pianistkevinjang@gmail.com>
2017-01-07 10:13:19 -0800

    Add specRunner to npm start script and remove comment of functional App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,3 @@
-// var App = () => (
-//   <div>
-//     <Nav />
-//     <div className="col-md-7">
-//       <VideoPlayer video={exampleVideoData[0]} />
-//     </div>
-//     <div className="col-md-5">
-//       <VideoList videos={exampleVideoData} />
-//     </div>
-//   </div>
-// );
-
 class App extends React.Component {
   constructor(props) {
     super(props);
