cf8140ed5f1227a859cf62dbc9e75a92dc532106
Kevin Jang <pianistkevinjang@gmail.com>
2017-01-07 10:58:28 -0800

    Implement searchYouTube and made it and youtube api key available in index.html

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,36 @@
+var searchObject = {
+  max: 5,
+  key: YOUTUBE_API_KEY,
+  query: 'react'
+}
+
 var searchYouTube = (options, callback) => {
-  // TODO
+
+   $.ajax({
+    data: {
+      part: 'snippet,id',
+      type: 'video',
+      videEmbeddable: true,
+      maxResults: options.max,
+      key: options.key,
+      q: options.query
+    },
+    dataType: 'json',
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    success: function(data) {
+      // console.log(data.items);
+      callback(data.items);
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  });
+
 };
 
 window.searchYouTube = searchYouTube;
+window.searchObject = searchObject;
+
+//options object: expected input to search
+  //callback =
